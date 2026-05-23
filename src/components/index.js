01f57f2b22b4f1ff6adc29f42404c2c5399b9785
Sameeratweb <sameermahammaddesai786@gmail.com>
2026-05-23 15:57:28 +0530

    Fixed import issue

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import CustomButton from "./CustomButton";
-import AIPicker from "./AIpicker";
+import AIPicker from "./AIPicker";
 import ColorPicker from "./ColorPicker";
 import FilePicker from "./FilePicker";
 import Tab from "./Tab"
